59ba1f35b0710894c3b6544a0e13a60b13857a24
Gailpositive <stormbrain2000@gmail.com>
2022-09-08 14:07:21 -0700

    Ghnj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+			puts("\"Programming is like building a multilingual puzzle");
+						return (0);
 }
