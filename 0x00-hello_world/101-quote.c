7b43bb3973fdf4e1c7f2c0bea4da6544400ffbe6
Gailpositive <stormbrain2000@gmail.com>
2022-09-08 21:09:35 -0700

    Hktf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 /**
  *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
+ *                followed by a new line, to standard error.
  *    *
  *     * Return: Always 1.
  */
