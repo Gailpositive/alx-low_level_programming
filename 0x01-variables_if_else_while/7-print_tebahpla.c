21bf598b5b9b6ddd57f5fb9085348543c6342c3f
Gailpositive <stormbrain2000@gmail.com>
2022-09-09 12:05:13 -0700

    Gjg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints the lowercase alphabet in reverse
+    *
+    * Return: 0 - Success
+      */
+int main(void)
+{
+		int i;
+
+			for (i = 122; i > 96; i--)
+					{
+								putchar(i);
+									}
+				putchar('\n');
+					return (0);
+}
