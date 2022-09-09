bb0d0ca88e2dacc7186570fbbc961d64de1f49f1
Gailpositive <stormbrain2000@gmail.com>
2022-09-08 21:01:05 -0700

    Jhff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",                            
+ *  *   *           followed by a new line, to standard error.
  *    *
  *     * Return: Always 1.
  */
