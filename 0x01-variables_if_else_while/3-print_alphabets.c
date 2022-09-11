06a34604008b69c24adfa9afcbb413de7f5f0268
Gailpositive <stormbrain2000@gmail.com>
2022-09-11 09:14:45 -0700

    Agnfjrjrt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,33 +1,26 @@
-#ilphabets.ciinclude <stdio.h>
+#include <stdio.h>
 
 /**
- *   main - prints out all possible combos of 3 digits
- *   Return: 0
+ *  main - Entry point
+ * 
+ *  Return: 0
 */
 int main(void)
-{
-	int i, j, k;
 
-	for (i = 48; i < 58; i++)
 {
-	for (j = i; j < 58; j++)
+	char a = 'a';
+	while (a <= 'z')
 {
-	for (k = j; k < 58; k++)
-{
-	if (i == j || j == k || i == k)			{
-	continue;					}	putchar(i);
-	putchar(j);
-	putchar(k);
-	if (i == 55 && j == 56 && k == 57)		{								break;	
+	putchar(a);
+	a++;
 }
-	else
-	putchar(',');
-																																																												putchar(' ');
-																																																																}
 
-																																															}
-																	}
-									}
-				putchar('\n');
-					return (0);
+	a = 'A';
+
+	while (a <= 'Z')
+{
+	putchar(a);
+	a++;						}	
+	putchar('\n');
+	return (0);
 }
