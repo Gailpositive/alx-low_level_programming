f6d30c656d165076ef05e51d04a7a3fa2cb3c3e1
Gailpositive <stormbrain2000@gmail.com>
2022-09-11 07:42:42 -0700

    Jahsgdh

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,13 @@
 */
 int main(void)
 {
-^
 	int i, j;
 	for (i = 48; i < 58; i++)
-{								for (j = i; j < 58; j++)			{
+        {
+	for (j = i; j < 58; j++)
+        { 
 	if (i == j)
-{
+        {
 	continue;
 }
 	putchar(i);
