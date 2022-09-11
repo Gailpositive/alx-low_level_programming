2f3e0eda11368d7239e4cee67e9bec65272fd4ff
Gailpositive <stormbrain2000@gmail.com>
2022-09-11 07:19:54 -0700

    Ghfeyj

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,21 @@ int main(void)
 	for (i = 48; i < 58; i++)
 {								for (j = i; j < 58; j++)			{
 	if (i == j)
-{								continue;					}							
-	putchar(i);						putchar(j);
-	if (i == 56 && j == 57)			
-{	
-	break;						}								else						{								putchar(',');	
+{
+	continue;
+}
+	putchar(i);
+	putchar(j);
+	if (i == 56 && j == 57)
+{
+	break;
+}
+	else
+{
+	putchar(',');
 	putchar(' ');
-}							
-}			
+}
+}
 	putchar('\n');
 	return (0);
 }
