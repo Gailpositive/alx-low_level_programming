3cd120bf422895e8853e7f66aed31be13e66f86d
Gailpositive <stormbrain2000@gmail.com>
2022-09-11 09:41:40 -0700

    Fhjolnbb

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	char b;
+	for (b = 'a'; b <= 'z'; b++)
+	if (b != 'q' && b != 'e')
+	putchar(b);
+	putchar('\n');
+	return (0);i
+}
