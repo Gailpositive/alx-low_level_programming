87c2847396dc017ec843897214a21c8a9b4c7997
Gailpositive <stormbrain2000@gmail.com>
2022-09-12 06:50:15 -0700

    Abdkfj

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - prints out all the numbers between 00 and 99
-* with no two digits being the same
+* main - main entry
+* * Description: This program prints the alphabets in lower case
 * Return: 0
 */
 int main(void)
 {
-	int i, j;
-	for (i = 48; i < 58; i++)
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
 {
-	for (j = i; j < 58; j++)
-{ 
-	if (i == j)
-{
-	continue;
-}
-	putchar(i);
-	putchar(j);
-	if (i == 56 && j == 57)
-{
-	break;
-}
-	else
-{
-	putchar(',');
-	putchar(' ');
-}
+	putchar(alphabet);				}
 	putchar('\n');
 	return (0);
 }
