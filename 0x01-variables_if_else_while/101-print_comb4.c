bde3f54cf508e2354db62974e9ad5eb6daa91579
Gailpositive <stormbrain2000@gmail.com>
2022-09-13 02:50:07 -0700

    Svjih

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,21 +13,23 @@ int main(void)
 	int c;
 
 	for (a = 0; a <= 9; a++)
-{
+	{
 
 	for (b = 0; b <= 9; b++)
-{
+	{
 
 	for  (c = 0; c <= 9; c++)
-{
+	{
 
 	if (c > b && b > a)
-{
+	{
+
 	putchar(a + '0');
 	putchar(b + '0');
 	putchar(c + '0');
 	if (a != 7 || b != 8 || c != 9)
-{
+	{
+
 	putchar(',');
 	putchar(',');
 }
