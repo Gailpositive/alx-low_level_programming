050c7ab6972b3323befdc5a51f37bbe3a6f3d81d
Gailpositive <stormbrain2000@gmail.com>
2022-09-13 15:10:23 -0700

    Khfwj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,12 @@
 void print_alphabet(void)
 {
 
-	int j;
+     int j;
 
-	for (j = 'a'; j <= 'z'; j++)
-	{
-	    _putchar(j);
-	}
-	_putchar('\n');
+     for (j = 'a'; j <= 'z'; j++)
+     {
+	  _putchar(j);
+     }
+     _putchar('\n');
 }
 
