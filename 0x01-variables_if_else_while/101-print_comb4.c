0ee70139b03ea6da0946c0de6a18e4546e05de4d
Gailpositive <stormbrain2000@gmail.com>
2022-09-13 03:09:37 -0700

    Absjfh

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 * main - entry point
 *
 * main - three digits
-*   
+*
 * Return: 0
 */
 int main(void)
@@ -15,9 +15,14 @@ int main(void)
 	for (a = 0; a <= 9; a++)
 {
 	for (b = 0; b <= 9; b++)
-{								for (c = 0; c <= 9; c++)
-{								if (c > b && b > a)				{								putchar(a + '0');					putchar(b + '0');					putchar(c + '0');					if (a != 7 || b != 8 || c !=9) 			{
-	putchar(',');						putchar(' ');					}							}
+{
+	for (c = 0; c <= 9; c++)
+{
+	if (c > b && b > a)				{	putchar(a + '0');					putchar(b + '0');					putchar(c + '0');					if (a != 7 || b != 8 || c !=9) 			{
+	putchar(',');
+	putchar(' ');
+}
+}
 }
 }
 }
