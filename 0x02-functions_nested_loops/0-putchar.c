3fe2df67d7c8553b29ca3441e12c5da1cf8c0725
Gailpositive <stormbrain2000@gmail.com>
2022-09-13 16:45:16 -0700

    Aljp

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h";
 
-/** main - prints putchar
-* Return: always 0
+/** 
+* main - This program prints the phrase Hello, World
+* Return: always 0 (Success)
 */
 int main(void)
 {
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[] = "_putchar";
+	int i;
+	
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		_putchar(ch[i]);
+	}
+
 	_putchar('\n');
 
 	return (0);
