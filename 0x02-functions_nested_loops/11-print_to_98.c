d8f8829854348b0f4897df475064adcde9efddbb
Gailpositive <stormbrain2000@gmail.com>
2022-09-14 10:52:18 -0700

    Cnhf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,7 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
+
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
@@ -22,13 +23,11 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-	
 		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 				printf("%i, ", i);
 			else
-			
 				printf("%i", i);
 		}
 	}
