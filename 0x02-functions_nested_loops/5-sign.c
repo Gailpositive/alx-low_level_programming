e36ef182ef4e231e8da7465f5ba7ce520020752f
Gailpositive <stormbrain2000@gmail.com>
2022-09-14 02:35:14 -0700

    Xcvt

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,9 @@
  * print_sign - checks the sign of an int passed to is
  * and prints the sign (+ / -)
  * @n: The integer to check
+ *
  * Return: 1 if n is greater than zero, and prints +
- * * returns 0 if n is zero, and prints 0
+ * returns 0 if n is zero, and prints 0
  * returns -1 if n is less than zero, and prints -
  */
 int print_sign(int n)
