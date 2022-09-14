b3777f8d3bc67f4702f831e7a4c82ca1ad9cd335
Gailpositive <stormbrain2000@gmail.com>
2022-09-13 17:45:08 -0700

    Xbh

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
-* print_sign - checks the sign of an int passed to is
-* and prints the sign (+ / -)
-* @n: The integer to check
-* Return: 1 if n is greater than zero, and prints +
-* * returns 0 if n is zero, and prints 0
-* returns -1 if n is less than zero, and prints -
-*/
+ * print_sign - checks the sign of an int passed to is
+ * and prints the sign (+ / -)
+ * @n: The integer to check
+ * Return: 1 if n is greater than zero, and prints +
+ * * returns 0 if n is zero, and prints 0
+ * returns -1 if n is less than zero, and prints -
+ */
 int print_sign(int n)
 {
 	if (n > 0)
