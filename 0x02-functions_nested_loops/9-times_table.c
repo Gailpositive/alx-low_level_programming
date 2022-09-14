2ad76b3bbfaa8c4bbdff60662e3288d9094866a9
Gailpositive <stormbrain2000@gmail.com>
2022-09-14 14:23:23 -0700

    Avjhg

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,16 @@ void times_table(void)
 
 			if ((x * y) > 9)
 				_putchar(a + '0');
-							
+
 			_putchar(b + '0');
-				
+
 			/* Don't print commas after the 10th column */
 			if (y != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-					
+			
 			/* Add an extra space if printing single digits */
 			if (y == 9 && x == 0)
 				break;
