e04b7b78de7078abd670273466442a4d9cc442d5
Gailpositive <stormbrain2000@gmail.com>
2022-09-14 08:26:24 -0700

    Xbhf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,22 +16,24 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 				printf("%i, ", i);
-		else
+			else
 				printf("%i", i);
 		}
-}
-else if (n > 98)
-{
-	for (i = n; i >= 98; i--)
-	{
-		if (i != 98)
-			printf("%i, ", i);
-		else
-			printf("%i", i);
 	}
+	else if (n > 98)
+	{
+	
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%i, ", i);
+			else
+			
+				printf("%i", i);
+		}
+	}
+	else
+		printf("%i", n);
+
+	printf("\n")
 }
-else
-	printf("%i", n);
-
-printf("\n")
-
