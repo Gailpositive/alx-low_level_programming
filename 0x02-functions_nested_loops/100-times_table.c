6c73f2f99125a4f13fd12b999e95d68a7bc8b4d4
Gailpositive <stormbrain2000@gmail.com>
2022-09-14 08:47:57 -0700

    Abdiy

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+* three_dig - prints the series of characters if i * j is > 100
+* @n: number to analyze
+*
+* Return: nothing
+*/
+void three_dig(int n)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(n / 100 + '0');
+	if ((n / 10) % 10 <= 0)
+		_putchar('0');
+	else
+		_putchar((n % 100) / 10 + '0');
+	_putchar(n % 10 + '0');
+}
+/**
+* print_times_table - prints the n times table, starting with 0
+* @n: the number times table
+*
+* Return: nothing
+*/
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+				if (product > 99)
+				{
+					three_dig(product);
+					{
+					else if (product / 10 > 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(product / 10 + '0');
+						_putchar((product) % 10 + '0');
+
+					}
+					else if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(product % 10 + '0');
+					}
+					else
+						_putchar(product % 10 + '0');
+			}
+			_putchar('\n');
+}
