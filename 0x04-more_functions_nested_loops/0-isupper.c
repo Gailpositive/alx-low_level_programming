77dfb4a883162d8abff6e716ebcb9e8983b6d9d0
Gailpositive <stormbrain2000@gmail.com>
2022-09-15 06:30:48 -0700

    Vnkjgf

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-_isupper - checks is a character is uppercase
+* _isupper - checks is a character is uppercase
 * @c: carrier integer variable
 * Return: 1 if true, 0 if false
 */
@@ -12,3 +12,5 @@ int _isupper(int c)
 		return (1);
 	}
 }
+
+
