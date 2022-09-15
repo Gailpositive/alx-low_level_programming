4d8059dd2ea6987a4f1ea4f6a8aa2ed59b4ce6d9
Gailpositive <stormbrain2000@gmail.com>
2022-09-15 02:45:08 -0700

    Acbkkkh

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,15 @@
+
 #include "main.h"
+#include "main.h"
+
 /**
-* print_sign - prints the sign of a numver
+* print_sign - checks the sign of an int passed to is
+* and prints the sign (+ / -)
+* @n: The integer to check
 *
-* @n: the int to check
-*
-* Return: 1 and prints + if n is greater than zero
-*
-* 0 and prints 0 if n is zero
-*
-* -1 and prints - if n is less than zero
+* Return: 1 if n is greater than zero, and prints +
+* returns 0 if n is zero, and prints 0
+* returns -1 if n is less than zero, and prints -
 */
 int print_sign(int n)
 {
@@ -17,15 +18,10 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else
 	{
-		_putchar(48);
-		return (0);
-	}
-	else if (n == 0)
-	{
-		
 		_putchar('-');
+		return (-1);
 	}
-	return (-1);
 }
+
