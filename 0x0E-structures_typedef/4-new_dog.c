240191106b6a824910474a22d2a70578fafae341
Gailpositive <stormbrain2000@gmail.com>
2022-10-11 21:03:11 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,19 @@
 #include "dog.h"
-
 #include <stdlib.h>
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 /**
-* new_dog - function
-* @name: name of dog
-* @age: age of new doggo
-* @owner: owner of new doggo
-*
-* Description: function to create a new dog. Store copy of name & owner
-* Return: ptr, null if fail
+ * new_dog - function
+ * @name: name of dog
+ * @age: age of new doggo
+ * @owner: owner of new doggo
+ * Description: function to create a new dog. Store copy of name & owner
+ * Return: ptr, null if fail
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *nename, *neowner;
+
 	int lename, leowner;
 
 	dog_t *doggo = malloc(sizeof(dog_t));
@@ -30,7 +29,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	_strcpy(nename, name);
-
 	_strlen(owner);
 	leowner = _strlen(owner);
 	neowner = malloc((leowner + 1) * sizeof(char));
@@ -41,20 +39,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	_strcpy(neowner, owner);
-
 	doggo->name = nename;
 	doggo->age = age;
 	doggo->owner = neowner;
-
 	return (doggo);
 }
-
 /**
-* _strlen - function
-* @s: first operand & pointer
-*
-* Description: function that returns the length of a string
-* Return: Always 0
+ * _strlen - function
+ * @s: first operand & pointer
+ * Description: function that returns the length of a string
+ * Return: Always 0
 */
 int _strlen(char *s)
 {
@@ -68,12 +62,11 @@ int _strlen(char *s)
 	return (index);
 }
 /**
-* _strcpy - function
-* @src: copy from
-* @dest: copy to
-*
-* Description: copies strng pnted by src to dest with null
-* Return: char
+ * _strcpy - function
+ * @src: copy from
+ * @dest: copy to
+ * Description: copies strng pnted by src to dest with null
+ * Return: char
 */
 char *_strcpy(char *dest, char *src)
 {
@@ -85,4 +78,5 @@ char *_strcpy(char *dest, char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
+
 }
